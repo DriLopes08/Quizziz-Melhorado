88dc181e99422faa3925179ce6ca829875ace82b
DriLopes08 <141828047+DriLopes08@users.noreply.github.com>
2025-06-30 12:02:05 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,111 @@
+export const perguntas = [
+    {
+        enunciado: "Você acaba de receber a chance de participar de uma missão espacial para explorar um planeta desconhecido. Qual é seu sentimento inicial?",
+        alternativas: [
+            {
+                texto: "Sinto um pouco de medo, pois o desconhecido sempre traz riscos.",
+                afirmacao: [
+                    "Sua cautela demonstra sabedoria diante dos mistérios do cosmos.",
+                    "Você pondera sobre os perigos, mas também sente a grandiosidade do momento."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Estou ansioso para descobrir novos mundos e aprender mais sobre o universo.",
+                afirmacao: [
+                    "Sua curiosidade e coragem te impulsionam a explorar o desconhecido.",
+                    "Você sente que essa missão pode transformar sua visão da vida para sempre."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Durante a missão, você precisa escolher uma especialidade para ajudar a equipe. Qual você escolhe?",
+        alternativas: [
+            {
+                texto: "Astrônomo: estudar estrelas, planetas e galáxias.",
+                afirmacao: [
+                    "Seu olhar atento às estrelas pode desvendar segredos do universo.",
+                    "Sua paixão por entender o cosmos te guia com precisão nas decisões da missão.",
+                    "Você se sente conectado ao Universo enquanto interpreta seus sinais."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Engenheiro espacial: cuidar das tecnologias e equipamentos da nave.",
+                afirmacao: [
+                    "Sua habilidade técnica garante a segurança e sucesso da missão.",
+                    "Cada reparo seu mantém a esperança e estabilidade da tripulação.",
+                    "Seu conhecimento prático transforma ideias em soluções reais no espaço."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você descobre sinais de vida em um planeta distante, mas é incerto se é hostil ou amigável. O que você faz?",
+        alternativas: [
+            {
+                texto: "Procuro formas de comunicação pacífica para entender essa vida.",
+                afirmacao: [
+                    "Sua empatia e diplomacia abrem portas para novas amizades cósmicas.",
+                    "Você sente que diálogo é a chave para evitar conflitos interplanetários.",
+                    "Sua abordagem pacífica inspira confiança e respeito entre os membros da missão."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Mantenho a equipe em alerta máximo e me preparo para qualquer perigo.",
+                afirmacao: [
+                    "Sua prudência protege a equipe contra possíveis ameaças desconhecidas.",
+                    "Você garante que a segurança venha antes da curiosidade.",
+                    "Sua postura firme traz tranquilidade aos colegas em meio à incerteza."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O comandante da missão propõe uma exploração para um buraco negro próximo. Como você reage?",
+        alternativas: [
+            {
+                texto: "Aceito o desafio, pois estudar buracos negros pode revolucionar a ciência.",
+                afirmacao: [
+                    "Sua coragem científica pode levar a descobertas que mudam nossa visão do universo.",
+                    "Você sabe que o risco pode trazer uma recompensa incomparável."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Prefiro focar em áreas mais seguras e estudadas do espaço.",
+                afirmacao: [
+                    "Sua prudência ajuda a manter a missão estável e segura.",
+                    "Você acredita que há muito a explorar sem se arriscar tanto."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Ao retornar à Terra, você é convidado a compartilhar suas descobertas. Qual foco você escolhe para sua palestra?",
+        alternativas: [
+            {
+                texto: "A importância da exploração espacial para o futuro da humanidade.",
+                afirmacao: [
+                     "Você inspira outros a olhar para as estrelas e sonhar com o futuro.",
+                    "Suas palavras despertam vocações e renovam esperanças no progresso científico."
+                ],
+            },
+            {
+                texto: "Os mistérios do universo que ainda precisamos desvendar",
+                afirmacao: [
+                    "Você instiga a curiosidade e a sede pelo conhecimento na próxima geração.",
+                    "Sua palestra deixa claro que ainda há mais perguntas do que respostas no cosmos.",
+                    "O fascínio pelos enigmas do espaço acende chamas de pesquisa e investigação."
+                ],
+            },
+        ]
+    }
+] ;
